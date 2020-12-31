8973ad9b5567979e73e4442356a50f7917820aa2
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2020-12-31 17:22:55 +0530

    Version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 }
 
 plugins {
-    id("org.springframework.boot") version "2.3.4.RELEASE" apply false
+    id("org.springframework.boot") version "2.4.1" apply false
     id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
 
     kotlin("jvm") version "1.4.10" apply false
